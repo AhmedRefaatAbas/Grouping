d580e4fbea0397095d3805f1798c3d854dbac863
AhmedRefaatAbas <ahmed.refaat1@msa.edu.eg>
2022-05-25 12:53:37 +0200

    Add files via upload

---
+++ AssignScreensToGroups.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Grouping
+{
+    /// <summary>
+    /// Interaction logic for AssignScreensToGroups.xaml
+    /// </summary>
+    public partial class AssignScreensToGroups : Window
+    {
+        public StackPanel sp = new StackPanel();
+        public MainWindow Y;
+
+        public object Children { get; }
+
+        public AssignScreensToGroups(string NumGroup,string NumScreen,MainWindow X)
+        {
+            
+            Y = X;
+            InitializeComponent();
+            if(NumGroup == null)
+            {
+                NumGroup="5";
+            }
+            for (int i = 1;i < int.Parse(NumGroup) +1;i++)
+            {
+                
+                comboBox.Items.Add(i);
+            }
+            for (int i = 1; i < int.Parse(NumScreen) + 1; ++i)
+            {
+                Button button = new Button()
+                {
+                    Content = string.Format(i.ToString(), i),
+                    Tag = i,
+                    Background = Brushes.Aqua
+                    
+                };
+                if(Y.Ct > 0)
+                {
+                    for (int k = 1; k < int.Parse(Y.NumOfGroups) + 1; k++)
+                    {
+                        for (int j = 0; j < Y.groups[k - 1].ScreenId.Count; j++)
+                        {
+                            if (Y.groups[k - 1].ScreenId[j] == i)
+                            {
+                                button.Background = Brushes.Blue;
+                            }
+                        }
+                    }
+
+                }
+                
+                button.Click += new RoutedEventHandler(button_Click);
+                this.grid.Children.Add(button);
+            }
+            
+
+                
+        }
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (comboBox.SelectedIndex > -1)
+            {
+                Button conBut = (Button)sender;
+                string con = conBut.Content.ToString();
+
+
+                for (int i = 1; i < int.Parse(Y.NumOfGroups) + 1; i++)
+                {
+                    string t = comboBox.SelectedItem.ToString();
+                    if (Y.groups[i - 1].Group_Id == int.Parse(t))
+                    {
+
+                        if (conBut.Background == Brushes.Blue)
+                        {
+                            for (int k = 1; k < int.Parse(Y.NumOfGroups) + 1; k++)
+
+                                for (int j=0;j<Y.groups[k-1].ScreenId.Count;j++)
+                                {
+                                
+                                     if(Y.groups[k - 1].Group_Id.ToString()==t&& Y.groups[k - 1].ScreenId[j] ==int.Parse(con))
+                                     {
+                                        Y.groups[k-1].ScreenId.RemoveAt(j);
+                                        conBut.Background = Brushes.Aqua;
+                                        break;
+
+
+                                     }
+                                
+                                }
+                            
+                        }
+                        else
+                        {
+                            conBut.Background = Brushes.Blue;
+                            Y.groups[i - 1].ScreenId.Add(int.Parse(con));
+                        }
+
+                    }
+
+                }
+
+            }
+
+            //MessageBox.Show("ok");
+            //for (int i = 1; i < int.Parse(Y.NumOfGroups) + 1; i++)
+            //{
+
+            //    if (int.Parse(comboBox.SelectedItem.ToString()) == Y.groups[i - 1].Group_Id)
+            //    {
+            //        if (!conBut.IsEnabled && )
+            //        {
+
+            //        }
+            //    }
+            //}
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Grouping
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class group
+        {
+            public int Group_Id;
+            public List<int> ScreenId = new List<int>();
+        }
+        public class student
+        {
+           public string Name;
+           public string Email;
+           public string Password;
+           public int Group_Id;
+        }
+        public string NumOfScr = "50";
+        public string NumOfGroups;
+        public List<group> groups;
+        public List<student> students;
+        public int Ct = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string conString = "Data Source=localhost;Initial Catalog=ExampleDatabase;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            string selectSql = "select GroupID,Email,Password,Type,Screens from Grouping_Db Where Email=@Email";
+            SqlCommand cmd = new SqlCommand(selectSql, con);
+            cmd.Parameters.AddWithValue("Email","ahmed@on.com");
+            SqlDataReader reader;
+            reader = cmd.ExecuteReader();
+            ChooseNumberOfScreens X = new ChooseNumberOfScreens(this,NumOfScr);
+            X.Show();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(NumOfScr);
+            AssignScreensToGroups X = new AssignScreensToGroups(NumOfGroups,NumOfScr,this);
+            groups = new List<group>();
+            for(int i=1;i<int.Parse(NumOfGroups)+1;i++)
+            {
+                group V = new group();
+                V.Group_Id = i;
+                groups.Add(V);
+            }
+            Ct++;
+            X.Show();
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            for(int i =0; i < groups.Count;i++)
+            {
+                MessageBox.Show("Group"+groups[i].Group_Id.ToString());
+                for(int k = 0; k<groups[i].ScreenId.Count;k++ )
+                {
+                    MessageBox.Show("Screen"+groups[i].ScreenId[k].ToString());
+                }
+            }
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
